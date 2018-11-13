2f784004c2904ebe66490b36495c7bbc80895e13
Jusufadis Bakamovic <jbakam@gmail.com>
2018-11-13 18:24:34 +0100

    Tweak existing project-builder tests to include real cxxd-config.

--- tests/unit/test_service_project_builder.py
+++ tests/unit/test_service_project_builder.py
@@ -2,21 +2,24 @@ import os
 import unittest
 
 from file_generator import FileGenerator
+from parser.cxxd_config_parser import CxxdConfigParser
 
 class ProjectBuilderTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.file_to_be_built = FileGenerator.gen_simple_cpp_file()
+        cls.cxxd_config      = FileGenerator.gen_cxxd_config_filename()
         cls.project_root_directory = os.path.dirname(cls.file_to_be_built.name)
 
     @classmethod
     def tearDownClass(cls):
         FileGenerator.close_gen_file(cls.file_to_be_built)
+        FileGenerator.close_gen_file(cls.cxxd_config)
 
     def setUp(self):
         import cxxd_mocks
         from services.project_builder_service import ProjectBuilder
-        self.service = ProjectBuilder(self.project_root_directory, None, cxxd_mocks.ServicePluginMock())
+        self.service = ProjectBuilder(self.project_root_directory, CxxdConfigParser(self.cxxd_config.name), cxxd_mocks.ServicePluginMock())
         self.build_cmd = 'gcc -o {0}.o -c {1}'.format(self.file_to_be_built.name, self.file_to_be_built.name)
 
     def test_if_call_returns_true_for_success_file_containing_build_output_and_duration_when_run_on_existing_file(self):
