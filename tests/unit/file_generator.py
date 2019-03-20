7c35bad495a76ab348446db742bf341b96a77517
Jusufadis Bakamovic <jbakam@gmail.com>
2019-03-20 15:51:30 +0100

    File-generator: implement possibility to provide custom config section.
    Very handy for unit-testing various corner-cases of config-files.

--- tests/unit/file_generator.py
+++ tests/unit/file_generator.py
@@ -150,6 +150,13 @@ TabWidth: 4                             \n\
         return fd
 
     @staticmethod
+    def gen_cxxd_config_filename_with_invalid_section(sections):
+        fd = open(tempfile.gettempdir() + os.path.sep + '.cxxd_config.json', 'w', 0)
+        for section in sections:
+            fd.write(section)
+        return fd
+
+    @staticmethod
     def gen_cxxd_config_filename(target, config_path, config_type='compilation-database'):
         fd = open(tempfile.gettempdir() + os.path.sep + '.cxxd_config.json', 'w', 0)
         fd.write('\
@@ -189,6 +196,7 @@ TabWidth: 4                             \n\
          ]                                      \n\
     },                                          \n\
     "clang-tidy" : {                            \n\
+        "path": "clang-tidy",                   \n\
         "args": {                               \n\
             "-analyze-temporary-dtors" : true,  \n\
             "-explain-config" : false,          \n\
@@ -196,6 +204,7 @@ TabWidth: 4                             \n\
         }                                       \n\
     },                                          \n\
     "clang-format" : {                          \n\
+        "path": "clang-format",                 \n\
         "args": {                               \n\
             "-sort-includes" : true,            \n\
             "-style" : "llvm",                  \n\
